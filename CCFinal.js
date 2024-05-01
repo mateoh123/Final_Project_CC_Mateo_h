4081dd683dc62422fa21907c268c2c838bdd0d37
mateoh123 <mateoche76@gmail.com>
2024-05-01 00:54:05 -0400

    reorganizing

--- CCFinal.js
+++ CCFinal.js
@@ -1,179 +1,160 @@
-titlex = 70
-titley = 250
-story = " "
-hovercolorF = "#ffffff"
-hovercolorB = "#ffffff"
-hovercolorL = "#ffffff"
-hovercolorR = "#ffffff"
+titlex = 70;
+titley = 250;
+story = " ";
+hovercolorF = "#ffffff";
+hovercolorB = "#ffffff";
+hovercolorL = "#ffffff";
+hovercolorR = "#ffffff";
 
 //EVENTS VARIABLES
-gotFlashlight = false
+gotFlashlight = false;
+flashlightx = 500;
 
 //ROOM VARIABLES
-gameStart = false
-firstroomValue = false
-secondroomValue = false
+gameStart = false;
+firstroomValue = false;
+secondroomValue = false;
 
 function preload() {
     flashlight = loadImage("images/a9ab94b863f3af9479650fdee9a7f553.png");
 }
 
-
-
 function setup() {
-    createCanvas(700, 700)
-    background(100, 100, 100)
+    createCanvas(700, 700);
 
-    playButton = createButton("PLAY")
-    playButton.position(290, 380)
-    playButton.style('font-size', '50px')
-    playButton.mousePressed(firstRoom)
-
-
+    playButton = createButton("PLAY");
+    playButton.position(290, 380);
+    playButton.style("font-size", "50px");
+    playButton.mousePressed(firstRoom);
 }
 
 function draw() {
     //title
-    push()
-    fill("white")
-    textSize(100)
-    textFont("Lugrasimo")
-    text("House Escape", titlex, titley)
-    pop()
-
+    background(0);
+    push();
+    fill("white");
+    textSize(100);
+    textFont("Lugrasimo");
+    text("House Escape", titlex, titley);
+    pop();
 
     if (gameStart) {
-        noStroke()
+        noStroke();
         //text box
-        fill("#8a8484")
-        rect(0, 500, 750, 200)
-        stroke(0)
-
+        fill("#8a8484");
+        rect(0, 500, 750, 200);
+        stroke(0);
 
         //forward
-        fill(hovercolorF)
-        rect(300, 550, 100, 40)
+        fill(hovercolorF);
+        rect(300, 550, 100, 40);
         //backward
-        fill(hovercolorB)
-        rect(300, 620, 100, 40)
+        fill(hovercolorB);
+        rect(300, 620, 100, 40);
         //left
-        fill(hovercolorL)
-        rect(180, 590, 100, 40)
+        fill(hovercolorL);
+        rect(180, 590, 100, 40);
         //right
-        fill(hovercolorR)
-        rect(420, 590, 100, 40)
+        fill(hovercolorR);
+        rect(420, 590, 100, 40);
 
-        textFont("Oswald")
-        fill("#000000")
-        textSize(20)
-        text("FORWARD", 302, 575) //forward
-        text("LEFT", 205, 615) //left
-        text("RIGHT", 440, 615) //right
-        textSize(17)
-        text("BACKWARD", 301.5, 645) //backward
+        textFont("Oswald");
+        fill("#000000");
+        textSize(20);
+        text("FORWARD", 302, 575); //forward
+        text("LEFT", 205, 615); //left
+        text("RIGHT", 440, 615); //right
+        textSize(17);
+        text("BACKWARD", 301.5, 645); //backward
 
-        textFont("Oswald")
-        fill("#ffffff")
-        textSize(30)
-        text(story, 15, 530)
+        textFont("Oswald");
+        fill("#ffffff");
+        textSize(30);
+        text(story, 15, 530);
 
         //BUTTON HOVER COLORS
         if (mouseX > 300 && mouseX < 400 && mouseY > 550 && mouseY < 590) {
-            hovercolorF = "#d4d4d4"
-
-
+            hovercolorF = "#d4d4d4";
         } else {
-            hovercolorF = "#ffffff"
-
+            hovercolorF = "#ffffff";
         }
 
         if (mouseX > 300 && mouseX < 400 && mouseY > 620 && mouseY < 660) {
-            hovercolorB = "#d4d4d4"
-
+            hovercolorB = "#d4d4d4";
         } else {
-            hovercolorB = "#ffffff"
+            hovercolorB = "#ffffff";
         }
 
         if (mouseX > 180 && mouseX < 280 && mouseY > 590 && mouseY < 630) {
-            hovercolorL = "#d4d4d4"
-
+            hovercolorL = "#d4d4d4";
         } else {
-            hovercolorL = "#ffffff"
+            hovercolorL = "#ffffff";
         }
 
         if (mouseX > 420 && mouseX < 520 && mouseY > 590 && mouseY < 630) {
-            hovercolorR = "#d4d4d4"
-
+            hovercolorR = "#d4d4d4";
         } else {
-            hovercolorR = "#ffffff"
+            hovercolorR = "#ffffff";
         }
-
-        //BUTTON CLICK BOUNDARIES
-        if (mouseIsPressed === true) {
-            //FORWARD BUTTON BOUNDARIES
-            if (mouseX > 300 && mouseX < 400 && mouseY > 550 && mouseY < 590) {
-                if (gotFlashlight != true && firstroomValue) {
-                    story = "Its too dark! I cant move forward!"
-                }
-
-            }
-
-            //BACKWARD BUTTON BOUNDARIES
-            if (mouseX > 300 && mouseX < 400 && mouseY > 620 && mouseY < 660) {
-                if (gotFlashlight != true && firstroomValue) {
-                    story = "Theres nothing behind me..."
-                }
-
-            }
-
-            //LEFT BUTTON BOUNDARIES
-            if (mouseX > 180 && mouseX < 280 && mouseY > 590 && mouseY < 630) {
-                if (gotFlashlight != true && firstroomValue) {
-                    story = "Its too dark! I cant move left!"
-                }
-
-            }
-
-            //RIGHT BUTTON BOUNDARIES
-            if (mouseX > 420 && mouseX < 520 && mouseY > 590 && mouseY < 630) {
-                if (gotFlashlight != true && firstroomValue) {
-                    story = "Its too dark! I cant move right!"
-                }
-
-            }
-
-        }
-
     }
 
     //ROOM LOGIC
     if (firstroomValue) {
-        secondroomValue = false
+        secondroomValue = false;
+        image(flashlight, flashlightx, 400, 80, 30);
     } else if (secondroomValue) {
-        firstroomValue = false
+        firstroomValue = false;
     }
-
 }
 
 function firstRoom() {
-    background(0, 0, 0)
-    firstroomValue = true
-    gameStart = true
-    titlex = 1000
-    titley = 1000
-    playButton.remove()
-    story = "Where am I? Its so dark in here!"
-    image(flashlight, 500, 400, 80, 30);
-
-
-
-
+    background(0, 0, 0);
+    firstroomValue = true;
+    gameStart = true;
+    titlex = 1000;
+    titley = 1000;
+    playButton.remove();
+    story = "Where am I? Its so dark in here!";
 }
 
 function secondRoom() {
-    background(150, 150, 150)
-    secondroomValue = true
+    background(150, 150, 150);
+    secondroomValue = true;
+}
 
+function mousePressed() {
+    //FORWARD BUTTON BOUNDARIES
+    if (mouseX > 300 && mouseX < 400 && mouseY > 550 && mouseY < 590) {
+        if (gotFlashlight != true && firstroomValue) {
+            story = "Its too dark! I cant move forward!";
+        }
+    }
+
+    //BACKWARD BUTTON BOUNDARIES
+    if (mouseX > 300 && mouseX < 400 && mouseY > 620 && mouseY < 660) {
+        if (gotFlashlight != true && firstroomValue) {
+            story = "Theres nothing behind me...";
+        }
+    }
+
+    //LEFT BUTTON BOUNDARIES
+    if (mouseX > 180 && mouseX < 280 && mouseY > 590 && mouseY < 630) {
+        if (gotFlashlight != true && firstroomValue) {
+            story = "Its too dark! I cant move left!";
+        }
+    }
+
+    //RIGHT BUTTON BOUNDARIES
+    if (mouseX > 420 && mouseX < 520 && mouseY > 590 && mouseY < 630) {
+        if (gotFlashlight != true && firstroomValue) {
+            story = "Its too dark! I cant move right!";
+        }
+    }
+    //CLICKING BUTTON
+    if (mouseX > 500 && mouseX < 580 && mouseY > 400 && mouseY < 430 && firstroomValue) {
+        story = "I can see now!";
+        flashlightx = -100;
+    }
 
 
 }
